2ba92e1981ec332abc2ea0d0056edae20c820633
Samuels Tdm <samueljiemenez1411@gmail.com>
2025-05-14 02:22:10 -0500

    Added small redirect to github (Docs)

--- src/routes/router.js
+++ src/routes/router.js
@@ -22,6 +22,22 @@ async function router(fastify, options) {
 
   // Users
   await fastify.register(import("./users/usr.js"), { prefix: "/users" });
+
+  // Home
+  fastify.get("/", async (request, reply) => {
+    const html = `
+      <html>
+        <head>
+          <meta http-equiv="refresh" content="3;url=https://github.com/darkmoon1411z/simpleapis" />
+        </head>
+        <body style="font-family: sans-serif; text-align: center; margin-top: 20%;">
+          <h2>Redirigiendo a la nueva ruta...</h2>
+          <p>Seras redirigido en unos segundos. Si no lo eres, haz clic <a href="https://github.com/darkmoon1411z/simpleapis">aqui</a>.</p>
+        </body>
+      </html>
+    `;
+    reply.code(200).header("Content-Type", "text/html").send(html);
+  });
 }
 
 export default router;
